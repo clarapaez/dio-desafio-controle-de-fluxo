547a5c19ebe1f249dc43d08bb61f796324a713cb
Clara <claramanuela.paez@gmail.com>
2024-08-31 15:43:51 -0300

    adicionando resposta ao desafio

---
+++ src/desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package desafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+	
+	public ParametrosInvalidosException() {};
+	
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+	
+
+}
